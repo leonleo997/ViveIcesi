cadb1811b3360414540eac38088661053e620937
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2018-11-30 23:08:39 -0500

    Notificación sin firebase, Label: Menú Bienestar cambiado a ViveIcesi

---
+++ app/src/main/java/co/highusoft/viveicesi/NotificationService.java
@@ -0,0 +1,74 @@
+package co.highusoft.viveicesi;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class NotificationService extends Service {
+
+
+    NotificationManager manager;
+    FirebaseDatabase db;
+    int idNotificacion = 1;
+
+    public static final String CHANNEL_ID = "aplicaciones20182";
+    public static final String CHANNEL_NAME = "Aplicaciones-2018-2";
+    public static final int CHANNEL_IMPORTANCE = NotificationManager.IMPORTANCE_HIGH;
+
+    @Override
+    public void onCreate() {
+        //Esto corre en el hilo principal
+        crearNotificacion();
+
+
+
+        //Si quieren descargar algo o hacer un network process
+        //Entonces iniciamos un HILO!!!!!!!!!!!
+
+    }
+
+    private void crearNotificacion() {
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Manejar notificaciones en OREO
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //Se tiene que definir un Notficacion Channel
+            //Crear 3 constantes: CHANNEL_ID, CHANNEL_NAME, CHANNEL_IMPORTANCE
+            NotificationChannel canal = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, CHANNEL_IMPORTANCE);
+            manager.createNotificationChannel(canal);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat
+                .Builder(this, CHANNEL_ID)
+                .setContentTitle("Ejemplo")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("Esto es un ejemplo de notificacion")
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        manager.notify(idNotificacion, builder.build());
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+
+
+}
+
--- app/src/main/java/co/highusoft/viveicesi/view/fragments/FragFuncionalidadesBienestar.java
+++ app/src/main/java/co/highusoft/viveicesi/view/fragments/FragFuncionalidadesBienestar.java
@@ -1,4 +1,4 @@
-package co.highusoft.viveicesi;
+package co.highusoft.viveicesi.view.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,39 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RatingBar;
 
+import co.highusoft.viveicesi.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link CalificacionActividades.OnFragmentInteractionListener} interface
+ * {@link FragFuncionalidadesBienestar.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link CalificacionActividades#newInstance} factory method to
+ * Use the {@link FragFuncionalidadesBienestar#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalificacionActividades extends Fragment {
+public class FragFuncionalidadesBienestar extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    private static RatingBar ratingBarp1;
-    private static RatingBar ratingBarp2;
-    private static RatingBar ratingBarp3;
-    private static RatingBar ratingBarp4;
-    private static RatingBar ratingBarp5;
-    private static RatingBar ratingBarp6;
-    private static RatingBar ratingBarp7;
-    private static RatingBar ratingBarp8;
-    private static RatingBar ratingBarp9;
-    private static RatingBar ratingBarp10;
-    private static RatingBar ratingBarp11;
-
-    private Button buttonGuardarEncuesta;
-
-
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -47,7 +30,7 @@ public class CalificacionActividades extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public CalificacionActividades() {
+    public FragFuncionalidadesBienestar() {
         // Required empty public constructor
     }
 
@@ -57,11 +40,11 @@ public class CalificacionActividades extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CalificacionActividades.
+     * @return A new instance of fragment FragFuncionalidadesBienestar.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalificacionActividades newInstance(String param1, String param2) {
-        CalificacionActividades fragment = new CalificacionActividades();
+    public static FragFuncionalidadesBienestar newInstance(String param1, String param2) {
+        FragFuncionalidadesBienestar fragment = new FragFuncionalidadesBienestar();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -78,49 +61,11 @@ public class CalificacionActividades extends Fragment {
         }
     }
 
-
-
-
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View viewinflate = inflater.inflate(R.layout.fragment_calificacion_actividades, container, false);
-
-
-        ratingBarp1 = viewinflate.findViewById(R.id.pregunta1);
-        ratingBarp2 = viewinflate.findViewById(R.id.pregunta2);
-        ratingBarp3 = viewinflate.findViewById(R.id.pregunta3);
-        ratingBarp4 = viewinflate.findViewById(R.id.pregunta4);
-        ratingBarp5 = viewinflate.findViewById(R.id.pregunta5);
-        ratingBarp6 = viewinflate.findViewById(R.id.pregunta6);
-        ratingBarp7 = viewinflate.findViewById(R.id.pregunta7);
-        ratingBarp8 = viewinflate.findViewById(R.id.pregunta8);
-        ratingBarp9 = viewinflate.findViewById(R.id.pregunta9);
-        ratingBarp10 = viewinflate.findViewById(R.id.pregunta10);
-        ratingBarp11 = viewinflate.findViewById(R.id.pregunta11);
-
-buttonGuardarEncuesta.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        //guardar encuesta en base de datos, según la actividad
-    }
-});
-
-        //para obtener el valor de la calificación
-        //ratingbar1.getRating();
-
-
-
-
-
-        return  viewinflate;
-
+        return inflater.inflate(R.layout.fragment_frag_funcionalidades_bienestar, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
