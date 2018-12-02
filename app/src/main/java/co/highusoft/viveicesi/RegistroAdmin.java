c3ff4dbef21bb7c501a120a3a86cfc2aa25d7bd6
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2018-12-02 18:32:48 -0500

    xml registro admin y xml datos google

---
+++ app/src/main/java/co/highusoft/viveicesi/RegistroAdmin.java
@@ -0,0 +1,13 @@
+package co.highusoft.viveicesi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_admin);
+    }
+}
